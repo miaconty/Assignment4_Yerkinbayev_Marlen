993060cfd2ce8bff847ccf8965fe6a0f99a106e0
Assignment1_Yerkinbayev_Marlen_SE2518 <yerkinbayev.marlen@gmail.com>
2026-02-01 02:49:49 +0500

    feat(utils): add ReflectionUtils (RTTI) and SortingUtils (Lambdas)

--- src/utils/ReflectionUtils.java
+++ src/utils/ReflectionUtils.java
@@ -1,4 +1,25 @@
 package utils;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class ReflectionUtils {
+    public static void inspectClass(Class<?> clazz) {
+        System.out.println("=== Reflection Analysis: " + clazz.getSimpleName() + " ===");
+
+        System.out.println("-> Fields:");
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            System.out.println("   " + field.getType().getSimpleName() + " " + field.getName());
+        }
+
+        System.out.println("-> Methods (First 5):");
+        Method[] methods = clazz.getDeclaredMethods();
+        Arrays.stream(methods)
+                .limit(5)
+                .forEach(m -> System.out.println("   " + m.getName()));
+
+        System.out.println("=============================================");
+    }
 }
--- src/utils/SortingUtils.java
+++ src/utils/SortingUtils.java
@@ -1,4 +1,10 @@
 package utils;
 
+import model.Course;
+import java.util.List;
+
 public class SortingUtils {
+    public static void sortCoursesByName(List<Course> courses) {
+        courses.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+    }
 }
